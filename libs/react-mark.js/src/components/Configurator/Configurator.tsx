e057479caa7fc4177fbea9638c3f7e20d949426f
Aakash Shah <subs@appsparkler.com>
2022-05-03 08:06:39 +0530

    chore: wip

--- libs/react-mark.js/src/components/Configurator/Configurator.tsx
+++ libs/react-mark.js/src/components/Configurator/Configurator.tsx
@@ -1,5 +1,5 @@
 import { Box, createTheme, ThemeProvider } from '@mui/material';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 // export const Configurator = () => {
 
@@ -10,21 +10,24 @@ export const Configurator = () => {
   const theme = createTheme({ palette: { mode: 'light' } });
   console.log({ theme });
   const mark = 'hello world';
-  const options = JSON.stringify(
-    { foo: 'Foo', bar: 'Bar', baz: 'Baz' },
-    null,
-    6
-  ).replace('}', `    }`);
+  const options = { foo: 'foo', ignore: ['h1', '.hello'] };
+  const optionsString = JSON.stringify(options, null, 4).replace('}', `  }`);
+  const optionsRender = useMemo(() => {
+    const showOptions = Object.keys(options).length > 0 ? true : false;
+    if (showOptions) {
+      return `
+  options={${optionsString}}
+/>`;
+    }
+    return `
+/>`;
+  }, []);
   return (
     <ThemeProvider theme={theme}>
       <Box bgcolor={'grey.200'} p={2}>
         <pre>{`
 <Marker 
-    mark="${mark}"
-    options={${options}}
-/>
-              
-              `}</pre>
+  mark="${mark}"${optionsRender}`}</pre>
         <pre>{JSON.stringify(theme, null, 2)}</pre>
       </Box>
     </ThemeProvider>
