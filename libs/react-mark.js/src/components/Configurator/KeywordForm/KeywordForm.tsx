f02a5e278c42ef9567d771b0fd48a9a6635aa496
Aakash Shah <subs@appsparkler.com>
2022-05-03 14:41:50 +0530

    chore: wip setup keyword form

--- libs/react-mark.js/src/components/Configurator/KeywordForm/KeywordForm.tsx
+++ libs/react-mark.js/src/components/Configurator/KeywordForm/KeywordForm.tsx
@@ -1,6 +1,39 @@
-import { Box } from '@mui/material';
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from '@mui/material';
 import React from 'react';
 
 export const KeywordForm = (props: {}) => {
-  return <Box {...props}>Keyword FOrm</Box>;
+  return (
+    <Box {...props} display="flex" flexDirection="column" gap={2}>
+      {/* ROW 1 */}
+      <Box display="flex" gap={2}>
+        <TextField label="Keyword" fullWidth />
+        <FormControl fullWidth>
+          <InputLabel>Accurracy</InputLabel>
+          <Select
+            onChange={console.log}
+            defaultValue="complimentary"
+            label="Accurracy"
+          >
+            <MenuItem selected value="partially">
+              partially
+            </MenuItem>
+            <MenuItem value="exactly">exactly</MenuItem>
+            <MenuItem value="complimentary">complimentary</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+      {/** ROW 2 */}
+      <Box display="flex" gap={2}>
+        <TextField label="Element" fullWidth />
+        <TextField label="Class name" fullWidth />
+      </Box>
+    </Box>
+  );
 };
