5504c457b6a092bb727408d08314d57b22797b85
Aakash Shah <subs@appsparkler.com>
2022-05-06 05:18:43 +0530

    chore: add validation for punctuations

---
+++ libs/common-utils/src/array-utils.ts
@@ -0,0 +1,22 @@
+import { StringOrNumber } from 'common-types';
+import { filter, find, map } from 'lodash/fp';
+
+/**
+ * Filters out items in an array
+ * @param id - the id of the item you want to filter out
+ * @returns A new array without the items having the `id`
+ */
+export const filterOutWithId = <T extends { id: StringOrNumber }>(
+  id: StringOrNumber
+) => filter<T>((item) => item.id !== id);
+
+export const updateItemWithMatchingId = <T extends { id: StringOrNumber }>(
+  updatedItem: T
+) =>
+  map<T, T>((item) =>
+    item.id === updatedItem.id ? { ...updatedItem } : { ...item }
+  );
+
+export const findById = <T extends { id: StringOrNumber }>(
+  id: StringOrNumber
+) => find<T>((item) => item.id === id);
