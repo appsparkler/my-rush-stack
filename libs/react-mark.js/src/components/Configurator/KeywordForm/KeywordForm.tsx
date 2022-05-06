1f2b3cad24946b561b44b3e7861edda96caee8e5
Aakash Shah <subs@appsparkler.com>
2022-05-06 20:16:35 +0530

    chore: wip grid

--- libs/react-mark.js/src/components/Configurator/KeywordForm/KeywordForm.tsx
+++ libs/react-mark.js/src/components/Configurator/KeywordForm/KeywordForm.tsx
@@ -1,6 +1,10 @@
 import {
   Box,
+  Checkbox,
   FormControl,
+  FormControlLabel,
+  FormGroup,
+  Grid,
   InputLabel,
   MenuItem,
   Select,
@@ -34,10 +38,10 @@ export const KeywordForm = (props = {}) => {
         </FormControl>
       </Horizontal>
       {/** ROW 2 */}
-      <Box display="flex" gap={2}>
+      <Horizontal gap={2}>
         <TextField label="Element" fullWidth size="small" />
         <TextField label="Class name" fullWidth size="small" />
-      </Box>
+      </Horizontal>
 
       {/* ROW 3 */}
       <Synonyms name="synonyms" />
@@ -51,6 +55,68 @@ export const KeywordForm = (props = {}) => {
         btnLabel="Add Exclusion"
         ariaLabelDelete="delete exclusion"
       />
+
+      {/* ROW 5 */}
+      <Excludes
+        name="ignorePunctuation"
+        title="Ignore Punctuations"
+        label="punctuation"
+        ariaLabelAdd="add punctuation to ignore"
+        btnLabel="Add Punctuation"
+        ariaLabelDelete="delete punctuation to ignore"
+      />
+
+      {/* ROW 6 */}
+      <Horizontal gap={2}>
+        <TextField
+          type="number"
+          label="IFrames Timeout"
+          fullWidth
+          size="small"
+        />
+        <TextField type="text" label="Wildcards" fullWidth size="small" />
+      </Horizontal>
+
+      {/* ROW 7 */}
+      <FormGroup>
+        <Box
+          display="grid"
+          gridTemplateColumns={[
+            'repeat(2, 1fr)',
+            'repeat(3, 1fr)',
+            'repeat(4, 1fr)',
+          ]}
+        >
+          <FormControlLabel
+            control={<Checkbox defaultChecked />}
+            label="Separate Word Search"
+          />
+          <FormControlLabel
+            control={<Checkbox defaultChecked />}
+            label="Diacritics"
+          />
+          <FormControlLabel
+            control={<Checkbox defaultChecked />}
+            label="IFrames"
+          />
+          <FormControlLabel
+            control={<Checkbox defaultChecked />}
+            label="Case Sensitive"
+          />
+          <FormControlLabel
+            control={<Checkbox defaultChecked />}
+            label="Ignore Joiners"
+          />
+          <FormControlLabel
+            control={<Checkbox defaultChecked />}
+            label="Across Elements"
+          />
+          <FormControlLabel
+            control={<Checkbox defaultChecked />}
+            label="Debug"
+          />
+        </Box>
+      </FormGroup>
     </Vertical>
   );
 };
