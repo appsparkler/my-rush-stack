aabf2b036b94bdd577bde8afe8b5377d85f42be1
Aakash Shah <subs@appsparkler.com>
2022-05-23 07:59:08 +0530

    chore: working build

--- @types/mark.js/index.d.ts
+++ @types/mark.js/index.d.ts
@@ -1,54 +1,20 @@
-declare module "mark.js" {
-  export type WildCards = "disabled" | "enabled" | "withSpaces";
+export * from "./mark-options";
+export * from "./mark-regexp-options";
+import { MarkOptions } from "./mark-options";
+import { RegExpMarkerOptions } from "./mark-regexp-options";
 
-  export type Accuracy = "partially" | "complimentary" | "exactly";
-
-  export type RegExpMarkerOptions = Partial<{
-    element: string;
-    className: string;
-    exclude: string[];
-    iframes: boolean;
-    iframesTimeout: number;
-    acrossElements: boolean;
-    ignoreGroups: number;
-    each: () => void;
-    filter: () => void;
-    noMatch: () => void;
-    done: () => void;
-    debug: boolean;
-    log: Console;
-  }>;
-
-  export type MarkOptions = Partial<{
-    /**
-     * An array with exclusion selectors. Matches inside these elements will be ignored. Example: "filter": ["h1", ".ignore"]
-     */
-    exclude: string[];
-    accuracy: Accuracy;
-    acrossElements: boolean;
-    caseSensitive: boolean;
-    className: string;
-    debug: boolean;
-    diacritics: boolean;
-    done: () => void;
-    each: (_markedDomElement: Element) => void;
-    element: string;
-    filter: (textNode: Element, term: string, numberOfMarks: number) => void;
-    iframes: boolean;
-    iframesTimeout: number;
-    ignoreJoiners: boolean;
-    ignorePunctuation: string[];
-    log: Console;
-    noMatch: () => void;
-    separateWordSearch: boolean;
-    synonyms: Record<string, string>;
-    wildcards: WildCards;
-  }>;
-
-  export default class MarkJS {
-    constructor(context: Element);
-    mark: (str: string | string[], options?: MarkOptions) => void;
-    markRegExp: (str: RegExp, options?: RegExpMarkerOptions) => void;
-    unmark: () => void;
-  }
+export default class MarkJS {
+  constructor(context: Element);
+  mark: (str: string | string[], options?: MarkOptions) => void;
+  markRegExp: (str: RegExp, options?: RegExpMarkerOptions) => void;
+  unmark: () => void;
 }
+
+// declare module "mark.js" {
+//   export default class MarkJS {
+//     constructor(context: Element);
+//     mark: (str: string | string[], options?: MarkOptions) => void;
+//     markRegExp: (str: RegExp, options?: RegExpMarkerOptions) => void;
+//     unmark: () => void;
+//   }
+// }
