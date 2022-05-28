074145567a2965c018272c3436ab01e3cad8162a
Aakash Shah <subs@appsparkler.com>
2022-05-28 12:44:54 +0530

    chore: wip

---
+++ types/mark.js/lib/index.d.ts
@@ -0,0 +1,17 @@
+export * from "./mark";
+export * from "./mark-regexp";
+export * from "./mark-ranges";
+import { MarkOptions } from "./mark";
+import { RangeMarkerItem, RangesMarkerOptions } from "./mark-ranges";
+import { RegExpMarkerOptions } from "./mark-regexp";
+/**
+ * @public
+ */
+export default class MarkJS {
+    constructor(context: Element);
+    mark: (str: string | string[], options?: MarkOptions) => void;
+    markRegExp: (str: RegExp, options?: RegExpMarkerOptions) => void;
+    unmark: () => void;
+    markRanges: (ranges: RangeMarkerItem[], options?: RangesMarkerOptions) => void;
+}
+//# sourceMappingURL=index.d.ts.map
