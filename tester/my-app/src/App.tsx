05fcc3bc547c7aaf60c629c5413d23ec0d64a773
Aakash Shah <subs@appsparkler.com>
2022-05-29 15:54:44 +0530

    chore: wip

---
+++ tester/my-app/src/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from "./logo.svg";
+import { Marker } from "react-mark.js";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Marker mark="Learn" options={{}}>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </Marker>
+    </div>
+  );
+}
+
+export default App;
