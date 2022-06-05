c6026f1b771c81fedf26acd4936e6bc102780235
Aakash Shah <subs@appsparkler.com>
2022-06-05 12:39:31 +0530

    chore: wip - update static folder and path

--- react-mark.js/sb-docs/.storybook/manager.js
+++ react-mark.js/sb-docs/.storybook/manager.js
@@ -1,12 +1,11 @@
 import { create } from "@storybook/theming";
-import LogoPNG from './react-mark-js-logo.png'
 import { addons } from "@storybook/addons";
 
 const DarkTheme = create({
     base: "dark",
     brandTitle: "My custom storybook",
-    brandUrl: "http://localhost:2000",
-    brandImage: LogoPNG
+    brandUrl: "/",
+    brandImage: "/react-mark-js-logo.png"
 });
 
 addons.setConfig({
