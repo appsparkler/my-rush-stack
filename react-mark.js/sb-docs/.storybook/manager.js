9f59664aaa9df4ee44ea98557eb848b7650ebf93
Aakash Shah <subs@appsparkler.com>
2022-06-11 14:00:59 +0530

    chore: setting up branding

--- react-mark.js/sb-docs/.storybook/manager.js
+++ react-mark.js/sb-docs/.storybook/manager.js
@@ -3,9 +3,9 @@ import { addons } from "@storybook/addons";
 
 const DarkTheme = create({
     base: "dark",
-    brandTitle: "My custom storybook",
-    brandUrl: "/",
-    brandImage: "/react-mark-js-logo.png"
+    brandTitle: "React Mark JS Docs",
+    brandUrl: "https://www.appsparkler.com/docs/react-mark-js/?path=/docs/introduction--single-string",
+    brandImage: "https://www.appsparkler.com/docs/react-mark-js/react-mark-js-logo.png"
 });
 
 addons.setConfig({
