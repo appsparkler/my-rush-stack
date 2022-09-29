f1788c40094802e8f7ce4b776a373ab781670689
Aakash Shah <subs@appsparkler.com>
2022-09-29 12:25:32 +0530

    chore: wip

--- hfn-checkins/report-generator-app/src/App.tsx
+++ hfn-checkins/report-generator-app/src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
+import { google } from "googleapis";
 import './App.css';
 
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+
 function App() {
+  useEffect(() => {
+    const sheets = google.sheets("v4");
+    return () => {
+    }
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -24,3 +33,7 @@ function App() {
 }
 
 export default App;
+function useEffect(arg0: () => () => void, arg1: never[]) {
+  throw new Error('Function not implemented.');
+}
+
